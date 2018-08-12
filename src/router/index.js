fa28ad48c24cdbab69c2ec69f2821b56b816dcb7
Max <kmd@tigrio.ru>
2018-08-12 13:34:23 +0200

    added sudoku view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import mainPage from '../pages/main/main'
 //Чтобы подключить страницу (не забудь прописать страницу в routes)
 //@import pageName from  'path to pageName'
 
@@ -14,7 +14,7 @@ export default new Router({
     {
       path: '/',
       name: '',
-      component: '',
+      component: mainPage,
     }
   ],
    mode: 'history'
